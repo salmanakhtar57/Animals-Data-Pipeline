52603d408783be01e29b222485bf99d3a798a930
Salman <salmanmaken57@gmail.com>
2025-09-15 03:00:03 +0500

    Taking JSON & Transforming field

--- app/apis/animals_endpoints.py
+++ app/apis/animals_endpoints.py
@@ -1,10 +1,22 @@
 from fastapi import APIRouter
+import json
 
 router = APIRouter()
 
-@router.get("/animals/v1/animals")
-def get_list_of_animals(animals):
-    return animals
+with open(r"D:\Backend\Tech Companies Tasks\Funsol Technologies Task\Animals-Data-Pipeline\app\apis\animal_details.json", "r") as file:
+    content = json.load(file)
+
+
+
+for animal in content:
+    print(animal)
+
+    # Function to fetch Animal Details 
+def fetch_animal_details():
+    pass
+# @router.get("/animals/v1/animals")
+# def get_list_of_animals(animals):
+#     return animals
 
 @router.get("/animals/v1/animals/<id>")
 def get_animal_by_id():
