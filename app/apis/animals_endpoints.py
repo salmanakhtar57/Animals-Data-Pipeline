688d1c9c1b750e1d9ddb87e618776f4e8884f967
Salman <salmanmaken57@gmail.com>
2025-09-15 04:33:43 +0500

    Updates

--- app/apis/animals_endpoints.py
+++ app/apis/animals_endpoints.py
@@ -2,14 +2,12 @@ from fastapi import APIRouter
 import requests, math
 from app.helper_functions.fetch_details import fetch_all_animals
 from app.helper_functions.transform_details import transform_fields
-from typing import List
-from app.schema.schema import PipelineResponse
 
 router = APIRouter()
 
 BASE_URL = "http://127.0.0.1:3123"  
 
-def post_animals_in_batches(animals: List[dict], batch_size: int = 100) -> int:
+def post_animals_in_batches(animals: list, batch_size: int = 100):
     total_batches = math.ceil(len(animals) / batch_size)
     posted = 0
 
@@ -18,17 +16,17 @@ def post_animals_in_batches(animals: List[dict], batch_size: int = 100) -> int:
         resp = requests.post(f"{BASE_URL}/animals/v1/home", json=batch)
         if resp.status_code == 200:
             posted += len(batch)
+
     return posted
 
-@router.post("/pipeline/run", response_model=PipelineResponse)
+@router.post("/animals/pipeline/run")
 def run_pipeline():
-    raw_animals_file = fetch_all_animals()
-    cleaned_animals = transform_fields(raw_animals_file)
+    raw_animals = fetch_all_animals()
+    cleaned_animals = transform_fields(raw_animals)
     posted_count = post_animals_in_batches(cleaned_animals)
 
-    return PipelineResponse(
-        status="done",
-        fetched_file=raw_animals_file,
-        fetched=len(cleaned_animals),
-        posted=posted_count
-    )
+    return {
+        "status": "done",
+        "fetched": len(raw_animals),
+        "posted": posted_count
+    }
