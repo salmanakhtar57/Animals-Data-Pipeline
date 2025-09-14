f65c7abb3085fb39496943094ab056ede5c2680b
Salman <salmanmaken57@gmail.com>
2025-09-15 03:18:37 +0500

    Friends convert from string to List

--- app/apis/animals_endpoints.py
+++ app/apis/animals_endpoints.py
@@ -1,19 +1,38 @@
 from fastapi import APIRouter
 import json
+from datetime import datetime, timezone
 
 router = APIRouter()
 
-with open(r"D:\Backend\Tech Companies Tasks\Funsol Technologies Task\Animals-Data-Pipeline\app\apis\animal_details.json", "r") as file:
-    content = json.load(file)
+
+# Function to Transform Variable 
+def transform_fields(file_path: str):
+    with open(file_path, "r") as file:
+        animals = json.load(file)
+
+    transformed = []
+
+    for animal in animals:
+        if "friends" in animal:
+            if animal["friends"] and type(animal["friends"]) is str:
+                seperated = animal["friends"].split(",")
+                cleaned = []
+                for f in seperated:
+                    f = f.strip()
+                    if f:
+                        cleaned.append(f)
+                animal["friends"] = cleaned
+            elif not animal["friends"]:
+                animal["friends"] = []
+
+        transformed.append(animal)
+
+    return transformed
+
+animals = transform_fields(r"D:\Backend\Tech Companies Tasks\Funsol Technologies Task\Animals-Data-Pipeline\app\apis\animal_details.json")
+print(animals)
 
 
-
-for animal in content:
-    print(animal)
-
-    # Function to fetch Animal Details 
-def fetch_animal_details():
-    pass
 # @router.get("/animals/v1/animals")
 # def get_list_of_animals(animals):
 #     return animals
